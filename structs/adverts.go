6fe10cf3b2fe9b1e013fc6cf2e1a0a1e7e26c8b6
josh <josh@thisisbliss.com>
2020-12-27 06:28:03 +0000

    fix go lint errors

--- structs/adverts.go
+++ structs/adverts.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 )
 
+// AdvertProviders stores adverts from all providers
 type AdvertProviders struct {
 	Providers []string `json:"providers"`
 	Adverts   []Advert `json:"adverts"`
 }
 
+// Adverts stores adverts from a single provider
 type Adverts struct {
 	Adverts []Advert `json:"adverts"`
 }
 
+// Advert store information about a single advert
 type Advert struct {
 	Provider    string `json:"provider"`
 	ID          string `json:"id"`
@@ -27,39 +30,45 @@ type Advert struct {
 	Image       string `json:"image"`
 }
 
+// AddAdvert function to append a advert to the array
 func (adverts *Adverts) AddAdvert(advert Advert) []Advert {
 	adverts.Adverts = append(adverts.Adverts, advert)
 	return adverts.Adverts
 }
 
+// Location stores location parameters
 type Location struct {
 	Longitude float64 `json:"longitude"`
 	Latitude  float64 `json:"latitude"`
 }
 
+// Make stores information about a make from a provider
 type Make struct {
 	Provider string `json:"provider"`
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 }
 
+// MakeProvider stores information about a make
 type MakeProvider struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
+// Model stores information about a model from a provider
 type Model struct {
 	Provider string `json:"provider"`
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 }
 
+// ModelProvider stores information about a model
 type ModelProvider struct {
-	ID string `json:"id"`
-	// Providers map[string]string `json:"providers"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
+// Sha256 generates unique token
 func Sha256(o interface{}) string {
 	h := sha256.New()
 	h.Write([]byte(fmt.Sprintf("%v", o)))
